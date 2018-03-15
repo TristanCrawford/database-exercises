b8cbf52d0e6b788b7996bd52f63f018a331e3a35
Tristan Crawford <tristanthtcrawford@gmail.com>
2018-03-15 13:55:37 -0500

    Album Seeder Exercise

---
+++ album_seeder.sql
@@ -0,0 +1,5 @@
+USE codeup_test_db;
+INSERT INTO albums (artist, name, release_date, genre, sales)
+VALUES ( 'Michael Jackson', 'Thriller', 1980, 'Pop,Rock,R&B', 66),
+       ( 'AC/DC', 'Back in Black', 1978, 'Hard rock', 50),
+       ( 'Pink Floyd', 'The Dark Side of the Moon', 1971, 'Progressive Rock', 45);
