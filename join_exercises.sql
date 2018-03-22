0a4e7f4b30dc6171825b5aae52bcca0bb23e07d0
Tristan Crawford <tristanthtcrawford@gmail.com>
2018-03-22 09:06:46 -0500

    Join Bonus

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,42 +1,45 @@
-use employees;
+USE employees;
 
 /* Current Department Managers */
-SELECT d.dept_name AS 'Department Name',CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
-  FROM employees AS e
-  JOIN dept_manager AS dm
-    ON dm.emp_no = e.emp_no
-  JOIN departments AS d
-    ON d.dept_no = dm.dept_no
- WHERE dm.to_date LIKE "9999-%";
+SELECT d.dept_name 'Department Name',CONCAT(e.first_name, ' ', e.last_name) 'Department Manager'
+FROM employees e
+JOIN dept_manager dm ON dm.emp_no = e.emp_no
+JOIN departments d ON d.dept_no = dm.dept_no
+WHERE dm.to_date > curdate();
 
 /* Current Female Department Managers */
-SELECT d.dept_name AS 'Department Name',CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
-  FROM employees AS e
-  JOIN dept_manager AS dm
-    ON dm.emp_no = e.emp_no
-  JOIN departments AS d
-    ON d.dept_no = dm.dept_no
- WHERE dm.to_date LIKE "9999-%"
-   AND e.gender = 'F';
+SELECT d.dept_name 'Department Name',CONCAT(e.first_name, ' ', e.last_name) 'Department Manager'
+FROM employees e
+JOIN dept_manager dm ON dm.emp_no = e.emp_no
+JOIN departments d ON d.dept_no = dm.dept_no
+WHERE dm.to_date > curdate()
+AND e.gender = 'F';
 
-/* TODO Customer Service Employee Titles */
-SELECT t.title AS 'Title',COUNT(t.title)
-  FROM titles AS t
-  JOIN dept_emp AS de
-    ON de.emp_no = t.emp_no
- WHERE de.dept_no = 'd009'
-   AND de.to_date LIKE '9999-%'
-   AND t.to_date LIKE '9999-%'
+/* Customer Service Employee Titles */
+SELECT t.title 'Title',COUNT(t.title)
+FROM titles t
+JOIN dept_emp de ON de.emp_no = t.emp_no
+WHERE de.dept_no = 'd009'
+AND de.to_date > curdate()
+AND t.to_date > curdate()
 GROUP BY t.title;
 
 /* Current Manager Salaries */
-SELECT d.dept_name AS 'Department',CONCAT(e.first_name, ' ', e.last_name) AS 'Name',s.salary AS 'Salary'
-  FROM employees as e
-  JOIN dept_manager as dm
-    ON dm.emp_no = e.emp_no
-  JOIN departments as d
-    ON d.dept_no = dm.dept_no
-  JOIN salaries as s
-    ON s.emp_no = e.emp_no
- WHERE dm.to_date LIKE '9999-%'
-   AND s.to_date LIKE '9999-%';
+SELECT d.dept_name 'Department',CONCAT(e.first_name, ' ', e.last_name) 'Name',s.salary 'Salary'
+FROM employees e
+JOIN dept_manager dm ON dm.emp_no = e.emp_no
+JOIN departments d ON d.dept_no = dm.dept_no
+JOIN salaries s ON s.emp_no = e.emp_no
+WHERE dm.to_date > curdate()
+AND s.to_date > curdate();
+
+/* Bonus */
+SELECT d.dept_name, e.first_name, e.last_name, m.first_name, m.last_name
+FROM employees e
+JOIN dept_emp de ON e.emp_no = de.emp_no
+JOIN departments d ON de.dept_no = d.dept_no
+JOIN dept_manager mn ON d.dept_no = mn.dept_no
+JOIN employees m ON mn.emp_no = m.emp_no
+WHERE de.to_date > curdate()
+AND mn.to_date > curdate()
+ORDER BY d.dept_name;
